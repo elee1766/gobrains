43ad331000857724eac1ffe351beedee4cefc888
a <a@tuxpa.in>
2024-12-19 14:02:31 -0600

    noot

--- pkg/lbradio/builder.go
+++ pkg/lbradio/builder.go
@@ -5,6 +5,40 @@ import (
 	"strconv"
 )
 
+type PromptItems []PromptItem
+
+type PromptItem struct {
+	Entity string   `json:"entity"`
+	Values []string `json:"values"`
+	Weight int      `json:"weight"`
+	Option string   `json:"options"`
+}
+
+func (r PromptItems) MarshalText() ([]byte, error) {
+	o := &bytes.Buffer{}
+	for pidx, v := range r {
+		o.WriteString(v.Entity)
+		o.WriteString(":(")
+		for idx, vv := range v.Values {
+			o.WriteString(vv)
+			if len(v.Values) > 1 && idx != len(v.Values)-1 {
+				o.WriteString(",")
+			}
+		}
+		o.WriteString(")")
+		if v.Weight > 0 {
+			o.WriteString(":" + strconv.Itoa(v.Weight))
+		}
+		if v.Option != "" {
+			o.WriteString(":" + v.Option)
+		}
+		if len(r) > 1 && pidx != len(r)-1 {
+			o.WriteString(" ")
+		}
+	}
+	return o.Bytes(), nil
+}
+
 type PromptBuilder PromptItems
 
 func (o *PromptBuilder) Add(Entity string, values ...string) *PromptBuilder {
@@ -41,38 +75,4 @@ func (o *PromptBuilder) AddWithOption(entity string, option string, values ...st
 func (o *PromptBuilder) String() string {
 	val, _ := (*PromptItems)(o).MarshalText()
 	return string(val)
-}
-
-type PromptItems []PromptItem
-
-func (r PromptItems) MarshalText() ([]byte, error) {
-	o := &bytes.Buffer{}
-	for pidx, v := range r {
-		o.WriteString(v.Entity)
-		o.WriteString(":(")
-		for idx, vv := range v.Values {
-			o.WriteString(vv)
-			if len(v.Values) > 1 && idx != len(v.Values)-1 {
-				o.WriteString(",")
-			}
-		}
-		o.WriteString(")")
-		if v.Weight > 0 {
-			o.WriteString(":" + strconv.Itoa(v.Weight))
-		}
-		if v.Option != "" {
-			o.WriteString(":" + v.Option)
-		}
-		if len(r) > 1 && pidx != len(r)-1 {
-			o.WriteString(" ")
-		}
-	}
-	return o.Bytes(), nil
-}
-
-type PromptItem struct {
-	Entity string   `json:"entity"`
-	Values []string `json:"values"`
-	Weight int      `json:"weight"`
-	Option string   `json:"options"`
 }
